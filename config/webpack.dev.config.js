10f464b74c48956867dac63936db2fbbeccef222
MadfishDT <lafefish@gmail.com>
2019-05-19 15:16:47 +0900

    webpack dev config add minimize disable

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,23 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/zlibt.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  devtool: 'source-map',
-  target: 'node',
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  output: {
-    filename: 'zlibt_dev.js',
-    path: path.resolve(__dirname, '../dist/dev')
-  }
+    entry: './src/zlibt.ts',
+    module: {
+        rules: [
+        {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }
+        ]
+    },
+    devtool: 'source-map',
+    target: 'node',
+    optimization: {
+        minimize: false
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'zlibt.dev.js',
+        path: path.resolve(__dirname, '../dist/dev')
+    }
 };
